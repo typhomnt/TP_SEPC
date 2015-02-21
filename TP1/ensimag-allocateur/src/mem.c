e3fc99b13421b0b6d6e45655ec08ab83000650e0
garcia1 <maxime.garcia1@ensimag.grenoble-inp.fr>
2015-02-21 15:22:11 +0100

    rectification pour l'allocation pas besoin de la classe math ; attention pour continuer il faut encore modifier l'allocation car on ne peut pas allouer une case trop petite < sizeof(*void) sinon la subdivision mènerai à des cases libres qui ne peuvent contenir une addresse

--- TP1/ensimag-allocateur/src/mem.c
+++ TP1/ensimag-allocateur/src/mem.c
@@ -3,7 +3,6 @@
  * This code is distributed under the GLPv3 licence. *
  * Ce code est distribué sous la licence GPLv3+.     *
  *****************************************************/
-#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -23,12 +22,25 @@ typedef struct block_list block_list;
 
 block_list *TZL[BUDDY_MAX_INDEX + 1];
 
-double 
-log2(double n)  
-{    
-    return log(n)/log(2);  
+static unsigned long 
+next_pow_2(unsigned long size){
+	unsigned long res = 1;
+	while(res < size){
+		res = 2*res;
+	}
+	return res;
 }
-
+//a factoriser car deux fois la même fonction
+static int
+next_pow_2_index(unsigned long size){
+	unsigned long res = 1;
+	int index = 0;
+	while(res < size){
+		res = 2*res;
+		index++;
+	}
+	return index;
+}
 
 static void 
 add_head(block_list* block, int size_index){
@@ -38,22 +50,22 @@ add_head(block_list* block, int size_index){
 }
 
 static void*
-subdivizion(unsigned long size, unsigned long requested_size){
-	if(TZL[(int)log2(size)] != NULL){
+subdivizion(unsigned long size, unsigned long requested_size, int index){
+	if(TZL[index] != NULL){
 		if(size == requested_size){
-			void *block = TZL[(int)log2(size)];
+			void *block = TZL[index];
 			//the block is no longer free
-			TZL[(int)log2(size)] = TZL[(int)log2(size)]->suivant;
+			TZL[index] = TZL[index]->suivant;
 			return block;
 		}
 		else {
 			//we delete TZL[(int)log2(size)] from the free blocks of log2(size)
-			TZL[(int)log2(size)] = TZL[(int)log2(size)]->suivant;
+			TZL[index] = TZL[index]->suivant;
 			//we add the buddy first
-			add_head((block_list*)((unsigned long)TZL[(int)log2(size)]^size/2), (int)log2(size/2));
+			add_head((block_list*)((unsigned long)TZL[index]^size/2), index - 1);
 			//then the block pointed by TZL[(int)log2(size)]
-			add_head(TZL[(int)log2(size)], (int)log2(size/2));
-			return subdivizion(size/2,requested_size);
+			add_head(TZL[index], index - 1);
+			return subdivizion(size/2,requested_size, index - 1);
 		}
 		
 	}
@@ -63,15 +75,15 @@ subdivizion(unsigned long size, unsigned long requested_size){
 	}
 	else {
 		//we try to find a free block of 2*size in order to subdivize him
-		return subdivizion(2*size, requested_size);
+		return subdivizion(2*size, requested_size, index + 1);
 	}
 }
 //size must be divisible by 2
 static void*
-get_block(unsigned long size){
+get_block(unsigned long size, int index){
 	if(size % 2 != 0)
 		return 0;
-	void *block = subdivizion(size, size);
+	void *block = subdivizion(size, size, index);
 	if(block == 0)
 		return 0;
 	return block;
@@ -101,14 +113,21 @@ mem_alloc(unsigned long size)
 {
   /*  ecrire votre code ici */
 	//the size must be divisible by 2
-	unsigned long size2 = pow(2,ceil(log2(size)));
-        return get_block(size2);
+	if(size > ALLOC_MEM_SIZE)
+		return 0;
+	unsigned long size2 = next_pow_2(size);
+	int pow_2_index = next_pow_2_index(size);
+        return get_block(size2,pow_2_index);
 }
 
 int 
 mem_free(void *ptr, unsigned long size)
 {
 	/* ecrire votre code ici */
+	/*attention on ne peut pas désalouer une zone mémoire de taille < sizeof(void*) sinon
+	 on ne pourrai pas stocker l'addresse de la prochaine case libre mais on peut tout de même faire:
+	si size < sizeof(void*) chercher si le buddy de cette case est libre ; si c'est le cas on fusionne les deux case
+	si la nouvelle taille de cette zone est suffisant */
 	return 0;
 }
 
